5f829aac8d01a874cabc836c341d9ab12e80d364
dogrest <582229649@qq.com>
2019-04-17 16:24:52 +0800

    first commint ,add some headfiles

---
+++ charpter4/Copy.c
@@ -0,0 +1,11 @@
+#include<sys/stat.h>
+#include<fcntl.h>
+
+#ifndef BUF_SIZE
+#define BUF_SIZE
+#endif
+int main(int argc,int*argv[])
+{
+    int inFd,outFd,openFd;
+    mode_t
+}
---
+++ tlpi.h
@@ -0,0 +1,11 @@
+#ifndef TLPI_H
+#define TLPI_H
+
+#include<sys/types>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+
+#include<>
